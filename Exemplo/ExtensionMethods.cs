3a22d8e4bf83475fad14c2bf627ab7722e95c87f
Pablo Juan <pablojuan@foryoursystem.com>
2014-09-16 10:23:37 -0300

    exemplo blog

---
+++ Exemplo/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class ExtensionMethods
+    {
+
+        public static int ToInt(this string valor)
+        {
+            return Convert.ToInt32(valor);
+        }
+    }
+}
---
+++ Exemplo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            string texto = Console.ReadLine();
+            int x = texto.ToInt();//Convert.ToInt32(texto);
+            Console.Write("O dobro do valor {0} é {1}", x, x * 2);
+            Console.ReadKey();  
+        }
+    }
+}
